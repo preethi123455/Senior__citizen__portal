e067cfa2d56315261be385d78e04c0440ed053db
preethi123455 <preethiusha007@gmail.com>
2025-12-05 14:54:22 +0530

    12

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,27 +1,41 @@
 // Navbar.js
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css';
 
 function Navbar() {
+  const navigate = useNavigate();
+
   return (
     <nav>
       <div className="navbar">
-        <a className="brand" href="./home">
-         <button onClick={() => navigate("/home")}>Back</button>
+
+        <div className="brand">
           Grocerly
-        </a>
+        </div>
+
         <div className="nav-links">
+          <button
+            className="nav-button"
+            onClick={() => navigate("/home")}
+          >
+            Back
+          </button>
+
           <a href="/about" className="nav-link">
             About
           </a>
+
           <a href="/contact" className="nav-link">
             Contact
           </a>
+
           <a href="/cart" className="nav-link">
             Cart
           </a>
         </div>
+
       </div>
     </nav>
   );
